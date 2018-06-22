fd41d3bd3b779311226a32552a56152fd682bea3
Dmitry Peshekhonov <devdimidved@gmail.com>
2018-06-22 21:16:24 +0300

    Initial commit

---
+++ src/main/java/learn/devdimidved/sbhibernatetransaction/SbHibernateTransactionApplication.java
@@ -0,0 +1,13 @@
+package learn.devdimidved.sbhibernatetransaction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbHibernateTransactionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SbHibernateTransactionApplication.class, args);
+    }
+
+}
---
+++ src/test/java/learn/devdimidved/sbhibernatetransaction/SbHibernateTransactionApplicationTests.java
@@ -0,0 +1,17 @@
+package learn.devdimidved.sbhibernatetransaction;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SbHibernateTransactionApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
+
