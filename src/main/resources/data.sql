76114155db1b232f92c8ca41ca55e75b513c30c2
Dmitry Peshekhonov <devdimidved@gmail.com>
2018-06-23 20:23:07 +0300

    Add SQL script to populate DB with some values during start-up

---
+++ src/main/resources/data.sql
@@ -0,0 +1,4 @@
+INSERT into ACCOUNT (ID, FULL_NAME, BALANCE) values (100, 'John Doe', 2500);
+INSERT into ACCOUNT (ID, FULL_NAME, BALANCE) values (110, 'Sarah Connor', 1000);
+INSERT into ACCOUNT (ID, FULL_NAME, BALANCE) values (120, 'Molly Parker', 3000);
+INSERT into ACCOUNT (ID, FULL_NAME, BALANCE) values (130, 'Bob Muller', 2000);
